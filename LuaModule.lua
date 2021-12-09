106d092dec5abd7719a36de1268d4d855b1c9fcc
alexandit <93397825+alexandit@users.noreply.github.com>
2021-12-09 09:00:40 -0500

    Update LuaModule.lua

--- LuaModule.lua
+++ LuaModule.lua
@@ -1,1 +1,27 @@
+local Lua = {}
 
+Lua.strings = {}
+Lua.numbers = {}
+Lua.tables = {}
+Lua.bools = {}
+
+do local strings = Lua.strings;
+  function strings.capitalize(s)
+    return string.upper(s:sub(1,1)) .. s:sub(2)
+  end
+  function strings.preprocess(s, m)
+    if m == "q1" then
+      s = string.gsub("\\([\"])", 
+      function(c)
+        return string.format("\\(%03d)", string.byte(c))
+      end)
+      return s
+    elseif m == "q2" then
+      s = string.gsub("\\(%d%d%d)",
+      function(c)
+        return string.format("\\(%d%d%d)", string.char(c))
+      end)
+    end
+  end
+end
+return Lua
