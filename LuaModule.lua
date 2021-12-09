cb0723767b3e9c67884f7f497c86c73c4e98d11d
alexandit <93397825+alexandit@users.noreply.github.com>
2021-12-09 09:10:25 -0500

    Update LuaModule.lua

--- LuaModule.lua
+++ LuaModule.lua
@@ -11,16 +11,15 @@ do local strings = Lua.strings;
   end
   function strings.preprocess(s, m)
     if m == "q1" then
-      s = string.gsub("\\([\"])", 
-      function(c)
-        return string.format("\\(%03d)", string.byte(c))
+      s= string.gsub(s,"\\(\")", function(c)
+        return string.format("\\%03d", string.byte(c))
       end)
       return s
     elseif m == "q2" then
-      s = string.gsub("\\(%d%d%d)",
-      function(c)
+      s = string.gsub(s, "\\(%d%d%d)",function(c)
         return "\\" .. string.char(c)
       end)
+      return s
     end
   end
 end
